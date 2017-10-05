2585a6d32126e77de45f40a14df343feb396ca3d
PC-ALEX_DELL <alexzhoucs@gmail.com>
2017-10-05 12:00:14 +0800

    new project

---
+++ arith0-timetest/Project1/main.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <iostream>
+#include <time.h>
+#include <math.h>
+
+using namespace std;
+
+void main()
+{
+	timetest();
+}
---
+++ arith0-timetest/Project1/timetest.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+double timetest(long LONG)
+{
+
+}
