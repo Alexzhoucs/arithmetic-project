42573577b1ef19ab383be5f7ba06b8be7ce2cd7a
PC-ALEX_DELL <alexzhoucs@gmail.com>
2017-10-10 12:51:37 +0800

    a try
    not successful

--- arith0-timetest/Project1/random-string.cpp
+++ arith0-timetest/Project1/random-string.cpp
@@ -4,6 +4,7 @@
 #include <time.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <iostream>
 
 using namespace std;
 
@@ -13,17 +14,18 @@ int randstr(long n)
 {
 	FILE *fp;
 	char a[LENGTH+1];			//∂‡“ªŒª°∞\0°±
-	char name[20] = "input02-*.txt";
+	char name[20] = "input17-*.txt";
 	int l, err, i;
 	srand((unsigned)time(NULL));
-	for (int j = 1; j <= 3; j++)
+	for (int k = 1; k <= 3; k++)
 	{
-		name[8] = j + 48;
+		name[8] = k + 48;
 		err = fopen_s(&fp, name, "w");
 		if (err != 0)
 			return (err);
 		for (long j = 0; j < n; j++)
 		{
+			//cout << "\r" << 100 * j * k / (3 * n) << "%     ";
 			l = 1 + (rand() % (LENGTH));
 			for (i = 0; i < l; i++)
 				a[i] = 97 + rand() % 26;
