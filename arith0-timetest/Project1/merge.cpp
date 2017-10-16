a982973a991be7504fb06df47db7d36bfd6ace6d
PC-ALEX_DELL <alexzhoucs@gmail.com>
2017-10-17 04:08:22 +0800

    find creater

--- arith0-timetest/Project1/merge.cpp
+++ arith0-timetest/Project1/merge.cpp
@@ -1,1 +1,24 @@
-//输入数组与数组规模，对数组进行排序，输出0为成功，1为失败。
+//输入数组与排序起止下标，对数组进行排序，输出0为成功，1为失败。
+
+int merge(char a[], long b, long e, long m)
+{
+
+	return(0);
+}
+
+
+int mergesort(char a[], long b,long e)
+{
+	int r1, r2, r3;
+	long m = (b + e) / 2;
+	if (b < e)
+	{
+		r1 = mergesort(a, b, m);
+		r2 = mergesort(a, m + 1, e);
+		r3 = merge(a, b, e, m);
+	}
+	if (r1 + r2 + r3)
+		return(1);
+	else
+		return(0);
+}
